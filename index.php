f9e36e31ee205020c1c6ec46c3a33651843df994
Tony Rouyer <tony.rouyer@hotmail.fr>
2020-06-15 16:25:54 +0200

    v2

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>partie 5 exercice 6</title>
+        <title>partie 5 exercice 7</title>
     </head>
     <body>
         <p> <?php $hautDeFrance['51'] = 'Marne'; ?> </p>
